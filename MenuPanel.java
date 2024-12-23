c0ee79ce80decd6b096b589b2d7afb194edb6497
mtonzz <147930504+mtonzz@users.noreply.github.com>
2024-12-23 20:52:04 +0700

    Update MenuPanel.java

--- MenuPanel.java
+++ MenuPanel.java
@@ -1,14 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+
 
 class MenuPanel extends JPanel {
-    JButton startButton, exitButton;
+    JButton startButton, leaderboardButton, exitButton;
 
     MenuPanel(JFrame frame) {
         this.setPreferredSize(new Dimension(GamePanel.SCREEN_WIDTH, GamePanel.SCREEN_HEIGHT));
         this.setBackground(Color.black);
-        this.setLayout(new GridLayout(3, 1));
+        this.setLayout(new GridLayout(4, 1));
 
         JLabel title = new JLabel("Snake Game", SwingConstants.CENTER);
         title.setFont(new Font("Ink Free", Font.BOLD, 50));
@@ -21,12 +21,20 @@ class MenuPanel extends JPanel {
             frame.validate();
         });
 
+        leaderboardButton = new JButton("Leaderboard");
+        leaderboardButton.setFont(new Font("Ink Free", Font.BOLD, 30));
+        leaderboardButton.addActionListener(e -> {
+            frame.setContentPane(new RankingPanel(frame));
+            frame.validate();
+        });
+
         exitButton = new JButton("Exit");
         exitButton.setFont(new Font("Ink Free", Font.BOLD, 30));
         exitButton.addActionListener(e -> System.exit(0));
 
         this.add(title);
         this.add(startButton);
+        this.add(leaderboardButton);
         this.add(exitButton);
     }
 }
